db28755d17196d5511dddfabdb41488455d8d201
Diego Griep <fale@diegogriep.com.br>
2019-01-14 15:29:09 -0200

    Map

---
+++ map/map.js
@@ -0,0 +1,8 @@
+let lesson = 'map';
+let title = document.querySelector('h1');
+let content = document.querySelector('#content');
+
+title.innerHTML = lesson;
+
+content.innerHTML = `O que aprender com ${lesson}`;
+
